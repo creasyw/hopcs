0cf2780fd33cc3bdd2463a7c38fd279be5383bb2
creasyw <creasywuqiong@gmail.com>
2013-04-15 16:06:09 -0500

    Add 3rd cumulant generation

--- cumest.py
+++ cumest.py
@@ -3,9 +3,6 @@ import numpy as np
 def cum2est (signal, maxlag, nsamp, overlap, flag):
     """
     CUM2EST Covariance function.
-       Should be involed via "CUMEST" for proper parameter checks.
-       y_cum = cum2est (y, maxlag, samp_seg, overlap,  flag)
-
          y: input data vector (column)
          maxlag: maximum lag to be computed
          samp_seg: samples per segment (<=0 means no segmentation)
@@ -38,6 +35,57 @@ def cum2est (signal, maxlag, nsamp, overlap, flag):
         y_cum = np.hstack((np.conjugate(y_cum[maxlag+1:0:-1]), y_cum))
     return y_cum
 
+
+def cum3est (signal, maxlag, nsamp, overlap, flag, k1):
+    """
+    CUM3EST Third-order cumulants.
+        y: input data vector (column)
+        maxlag: maximum lag to be computed
+        samp_seg: samples per segment
+        overlap: percentage overlap of segments
+        flag : 'biased', biased estimates are computed  [default]
+               'unbiased', unbiased estimates are computed.
+        k1: the fixed lag in c3(m,k1): see below
+        y_cum:  estimated third-order cumulant,
+                 C3(m,k1)  -maxlag <= m <= maxlag
+    """
+    minlag = -maxlag
+    overlap = overlap/100*nsamp
+    nadvance = nsamp - overlap
+    nrecord = (len(signal)-overlap)/(nsamp-overlap)
+
+    y_cum = np.zeros(maxlag*2+1, dtype=float)
+    ind = 0
+    nlags = 2*maxlag + 1
+
+    if flag == "biased":
+        scale = np.ones(nlags, dtype=float)/nsamp
+    elif flag == "unbiased":
+        lsamp = nsamp - abs(k1)
+        scale = range(lsamp-maxlag,lsamp+1) + range(lsamp-1, lsamp-maxlag-1, -1)
+        scale = np.ones(len(scale), dtype=float)/scale
+    else:
+        raise Exception("The flag should be either 'biased' or 'unbiased'!!")
+
+    for i in range(nrecord):
+        x = signal[ind:(ind+nsamp)]
+        x = x-float(sum(x))/len(x)
+        cx = np.conjugate(x)
+        z = x*0
+        if k1 >= 0:
+            z[:nsamp-k1] = x[:nsamp-k1]*cx[k1:nsamp]
+        else:
+            z[-k1:nsamp] = x[-k1:nsamp]*cx[:nsamp+k1]
+
+        y_cum[maxlag] = y_cum[maxlag] +  reduce(lambda m,n:m+n, z*x, 0)
+
+        for k in range(1, maxlag+1):
+            y_cum[maxlag-k] = y_cum[maxlag-k] + reduce(lambda m,n:m+n, z[k:nsamp]*x[:nsamp-k], 0)
+            y_cum[maxlag+k] = y_cum[maxlag+k] + reduce(lambda m,n:m+n, z[:nsamp-k]*x[k:nsamp], 0)
+        ind += nadvance
+
+    return y_cum*scale/nrecord
+
 def test ():
     # for tesating purpose
     # The right results are:
@@ -45,8 +93,14 @@ def test ():
     #           "unbiaed": [-0.12444965  0.36246791  1.00586945  0.36246791 -0.12444965]
     import scipy.io as sio
     y = sio.loadmat("matfile/demo/ma1.mat")['y']
-    print cum2est(y, 2, 128, 0, 'unbiased')
+    #print cum2est(y, 2, 128, 0, 'unbiased')
+
+    # For the 3rd cumulant:
+    #           "biased": [-0.18203039  0.07751503  0.67113035  0.729953    0.07751503]
+    #           "unbiased": [-0.18639911  0.07874543  0.67641484  0.74153955  0.07937539]
+    print cum3est(y, 2, 128, 0, 'unbiased', 1)
 
 if __name__=="__main__":
+    test()
 
 
