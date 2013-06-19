4a57f9f669fbb70bf9de311338b56b59837443f2
creasyw <creasywuqiong@gmail.com>
2013-06-19 00:17:13 -0500

    change command line option

--- impulse_response.py
+++ impulse_response.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-def moving_average(nt, vt, input):
+def moving_average(lag, vt, input):
     """
-    Simulate the MA system with the # of nt taps, the impulse response of each
+    Simulate the MA system with the # of lag, the impulse response of each
     tap is stored in the list vt. Input is signal with format of 1D numpy.ndarray.
+    Note that using lag rather than taps in the command line is to be
+    consistent with maest and cumest.
     """
-    assert len(vt) == nt, "The number of taps is not equal to the length of IR."
-    temp = np.hstack((np.zeros(nt), input))
-    return np.array([sum(temp[k:k-nt:-1]*vt) for k in range(nt, len(temp))])
+    taps = lag+1
+    assert len(vt) == taps, "The number of taps is not equal to the length of IR."
+    temp = np.hstack((np.zeros(taps), input))
+    return np.array([sum(temp[k:k-taps:-1]*vt) for k in range(taps, len(temp))])
 
 
 
