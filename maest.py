5601ce720fe2896854d650b5fc16bd3ca8bf9777
creasyw <creasywuqiong@gmail.com>
2013-07-09 23:08:21 -0500

    change 4th-order average for coress-correlation

--- maest.py
+++ maest.py
@@ -52,10 +52,11 @@ def maestx(y, pcs, q, norder=3,samp_seg=1,overlap=0):
         else:
             print 'MAEST: alternative solution b1 used'
     else:
-        if all(np.sign(b2) == np.sign(b1)):
-            b1 = np.sign(b1)* (abs(b1) + abs(b2)**(1./3))/2
-        else:
-            print 'MAEST: alternative solution b1 used'
+        b1 = np.sign(b2)* (abs(b1) + abs(b2)**(1./3))/2
+#        if all(np.sign(b2) == np.sign(b1)):
+#            b1 = np.sign(b1)* (abs(b1) + abs(b2)**(1./3))/2
+#        else:
+#            print 'MAEST: alternative solution b1 used'
     return np.hstack(([1], b1))
 
 
