cacf1c414553860d7540d8ee89b4530b3c23fa75
creasyw <creasywuqiong@gmail.com>
2013-07-11 23:46:07 -0500

    change batch file to include multiprocessing

--- batch.py
+++ batch.py
@@ -1,30 +1,34 @@
 import numpy as np
-from maest import maestx
-import sys
+import maest as ma
+import impulse_response as ir
+from multiprocessing import Process
 
-result3 = []
-result4 = []
-step = 512
-pcs = [2,3,5,7]
+def task(maorder, pcs, winsize, r):
+  f = open("pcs_montecarlo_ma%d_%d_%d.csv"%(maorder, winsize, int(''.join(map(str,pcs)))), 'w')
+  for i in range(r):
+    signal = np.load("/home/q80022617/work/data/exp_deviate_one_%d.npy"%(i))
+    receive = ir.moving_average(2, [1,-2.333, 0.677], signal)
+    temp = ma.maestx(receive, pcs, 2, maorder, winsize)
+    f.write('%s\n' % temp)
+    print temp
+  f.close()
 
-for k in range(0,10):
-    signal = np.load("data/exp_deviate_one_%d.npz.npy"%(k))
-    y = np.zeros(len(signal)-2)
-    b1 = -2.333
-    b2 = 0.667
-    for i in range(len(y)):
-        y[i] = signal[i+2]+b1*signal[i+1]+b2*signal[i]
+
+def main():
+  pcs = [1,1,2,3]
+  threads = []
+  #winsize = 256
+  r = 50
+  job = []
+  for maorder in range(3,5):
+    for i in range(7,12):
+      winsize = 2**i
+      job.append(Process(target=task, args=(maorder, pcs, winsize, r)))
+      job[-1].start()
+      print "winsize %s for ma(%s)" % (winsize, maorder)
+
+
+if __name__ == "__main__":
+  main()
     
-    result3.append(maestx(y, pcs, 2, 3, step))
-    result4.append(maestx(y, pcs, 2, 4, step))
-    sys.stdout.write("%sth data file is complete, 3rd-order=%s, 4th-order=%s\r"%(k,result3[-1],result4[-1]))
-    sys.stdout.flush()
-
-f = open("test_result.txt", 'w')
-for i in result3:
-    print>>f, i
-print>>f, "\n\n"
-for i in result4:
-    print>>f, i
-f.close()
 
