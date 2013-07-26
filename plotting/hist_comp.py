256beb5732a60cba9361af1e0d7a3378b150b4a4
creasyw <creasywuqiong@gmail.com>
2013-07-25 22:28:54 -0500

    simplify comparsion procedure

--- plotting/hist_comp.py
+++ plotting/hist_comp.py
@@ -5,32 +5,6 @@ import re
 import sys, os
 import numpy as np
 
-def percentage (m):
-    # standard deviation / original values
-    return np.sqrt(m[1::2,:])/ np.absolute(m[0::2,:])
-
-
-def calculte(filename):
-    finder = re.compile("\[|-?\d+\.\d*[eE]?[-+]?\d*|\]")
-    result = []
-    temp = []
-    with open(os.path.join(os.path.dirname(__file__), filename)) as datafile:
-        for row in datafile:
-            hit = finder.findall(row)
-            if "[" in hit:
-                hit.remove("[")
-                temp = hit
-                if "]" in hit:
-                    temp.remove("]")
-                    result.append(map(lambda x: float(x), temp))
-            elif "]" in hit:
-                hit.remove("]")
-                temp += hit
-                result.append(map(lambda x: float(x), temp))
-            else:
-                temp += hit
-
-    return np.array(result)[2::3,:]
 
 def to_percent(y, position):
     # Ignore the passed in position. This has the effect of scaling the default
@@ -44,15 +18,15 @@ def to_percent(y, position):
         return s + '%'
 
 
-def hist_plot(m, n):
-    x = np.arange(0,10)
-    w1 = np.array([m[k/2,1] if k%2==0 else m[k%2,2] for k in range(10)])
-    w2 = np.array([n[k/2,1] if k%2==0 else n[k%2,2] for k in range(10)])
+def hist_plot(m, n, p):
+    x = np.arange(0,7)
+    #w1 = np.array([m[k/2,1] if k%2==0 else m[k%2,2] for k in range(10)])
+    #w2 = np.array([n[k/2,1] if k%2==0 else n[k%2,2] for k in range(10)])
     common_params = dict(bins=10, range=(0,10), normed=False)
 
     #plt.hist((x,x), weights=(w1,w2), label=["PCS(3)", "PCS(4)"], **common_params)
     #plt.legend(loc=0)
-    plt.hist(x, weights=w2, **common_params)
+    plt.hist((x,x,x), weights=(m,n,p), **common_params)
 
     #ax2 = plt.twinx()
     #ax2.plot(x, w1/w2)
@@ -65,8 +39,14 @@ def hist_plot(m, n):
     plt.show()
 
 if __name__ == "__main__":
-    hos3 = calculte("rms_winsize_short_pcs123.txt")
-    hos4 = calculte("rms_winsize_short_pcs1123.txt")
-    hist_plot(hos3, hos4)
+    m3_long = [0.86089537,0.32352935,1.18092441,0.52197473,0.21109557]
+    b10000_long = [1.48848624, 0.67883205, 1.90313507, 0.9890348, 0.35486214]
+    bchmk_long = [0.66954667, 0.3340954, 0.66633891, 0.30589311, 0.1339124]
+    
+    m3_short = [0.35177937, 0.02182355]
+    b10000_short = [1.82210635,  0.1862351]
+    bchmk_short = [0.28251416, 0.01283398]
+    
+    hist_plot(m3_short+m3_long, b10000_short+b10000_long, bchmk_short+bchmk_long)
 
 
