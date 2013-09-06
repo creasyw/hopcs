64fde465db045db428c9ccf9188465a02252ad9e
creasyw <creasywuqiong@gmail.com>
2013-09-06 09:21:11 -0500

    change order testing

--- multiprocessing/test_order.py
+++ multiprocessing/test_order.py
@@ -1,98 +1,102 @@
 import numpy as np
-import maest as ma
-import cumxst as cx
+from cumxst import cumx
+from cumest import cumest
 import impulse_response as ir
 from multiprocessing import Process
 from multiprocessing import Pool
 
-def task_cx(pcs, taps, testing_order, winsize, r, slicing):
-  f = open("cx_order_test_%s_hos%d_%d_slice%d_%d.csv"%(testing_order, len(pcs), winsize, slicing, int(''.join(map(str,pcs)))), 'w')
-  for i in range(r):
-    signal = np.load("/home/creasy/workplace/data/exp_deviate_one_%d.npy"%(i))[:slicing]
-    receive = ir.moving_average(taps, signal)
-    temp = cx.cumx(receive, pcs, len(pcs), testing_order, winsize)
-    f.write('%s\n' % temp)
-    print temp
-  f.close()
+def task_cx(pcs, testing_order, winsize, r, slicing, snr, noise_type):
+    if snr > 100:
+        f = open("result/cx_testorder%s_hos%d_winsize%d_slice%d_snr%d.csv"%(testing_order, len(pcs), winsize, slicing, snr), 'w')
+        for i in range(r):
+            receive = np.load("temp/data_%d.npy"%(i))[:slicing]
+            temp = cumx(receive, pcs, len(pcs), testing_order, winsize)
+            f.write('%s\n' % temp)
+            print "snr=+inf, ", temp
+    elif noise_type=="white":
+        f = open("result/cx_testorder%s_hos%d_winsize%d_slice%d_white_snr%d.csv"%(testing_order, len(pcs), winsize, slicing, snr), 'w')
+        for i in range(r):
+            receive = np.load("temp/data_white_%d_%d.npy"%(snr, i))[:slicing]
+            temp = cumx(receive, pcs, len(pcs), testing_order, winsize)
+            f.write('%s\n' % temp)
+            print "white noise, snr=%d, "%(snr), temp
+    elif noise_type=="color":
+        f = open("result/cx_testorder%s_hos%d_winsize%d_slice%d_color_snr%d.csv"%(testing_order, len(pcs), winsize, slicing, snr), 'w')
+        for i in range(r):
+            receive = np.load("temp/data_color_%d_%d.npy"%(snr, i))[:slicing]
+            temp = cumx(receive, pcs, len(pcs), testing_order, winsize)
+            f.write('%s\n' % temp)
+            print "color noise, snr=%d, "%(snr), temp
+    else:
+        print "ERROR: the noise type is wrong!!"
+    f.close()
 
-
-def task_ma(pcs, taps, testing_order, winsize, r, slicing):
-  f = open("ma_order_test_%s_hos%d_%d_slice%d_%d.csv"%(testing_order, len(pcs), winsize, slicing, int(''.join(map(str,pcs)))), 'w')
-  for i in range(r):
-    signal = np.load("/home/creasy/workplace/data/exp_deviate_one_%d.npy"%(i))[:slicing]
-    receive = ir.moving_average(taps, signal)
-    temp = ma.maestx (receive, pcs, testing_order, len(pcs), winsize)
-    f.write('%s\n' % temp)
-    print temp
-  f.close()
-
+def task_cm(hos_order, testing_order, winsize, r, slicing, snr, noise_type):
+    if snr > 100:
+        f = open("result/cm_testorder%s_hos%d_winsize%d_slice%d_snr%d.csv"%(testing_order, hos_order, winsize, slicing, snr), 'w')
+        for i in range(r):
+            receive = np.load("temp/data_%d.npy"%(i))[:slicing]
+            temp = cumest(receive, hos_order, testing_order, winsize)
+            f.write('%s\n' % temp)
+            print "snr=+inf, ", temp
+    elif noise_type=="white":
+        f = open("result/cm_testorder%s_hos%d_winsize%d_slice%d_white_snr%d.csv"%(testing_order, hos_order, winsize, slicing, snr), 'w')
+        for i in range(r):
+            receive = np.load("temp/data_white_%d_%d.npy"%(snr, i))[:slicing]
+            temp = cumest(receive, hos_order, testing_order, winsize)
+            f.write('%s\n' % temp)
+            print "white noise, snr=%d, "%(snr), temp
+    elif noise_type=="color":
+        f = open("result/cm_testorder%s_hos%d_winsize%d_slice%d_color_snr%d.csv"%(testing_order, hos_order, winsize, slicing, snr), 'w')
+        for i in range(r):
+            receive = np.load("temp/data_color_%d_%d.npy"%(snr, i))[:slicing]
+            temp = cumest(receive, hos_order, testing_order, winsize)
+            f.write('%s\n' % temp)
+            print "color noise, snr=%d, "%(snr), temp
+    else:
+        print "ERROR: the noise type is wrong!!"
+    f.close()
 
 
 def main():
-  job = Pool(8)
-  r = 50
-  winsize = 512
-  
-  taps = [1, -2.333, 0.667]
-  pcs = [1,2,3]
-  # test order mismatch
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_ma, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  pcs = [1,1,2,3]
-  # test order mismatch
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_ma, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  taps = [1, 0.1, -1.87, 3.02, -1.435, 0.49]
-  pcs = [1,2,3]
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_ma, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  pcs = [1,1,2,3]
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_ma, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  taps = [1, -2.333, 0.667]
-  pcs = [1,2,3]
-  # test order mismatch
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_cx, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  pcs = [1,1,2,3]
-  # test order mismatch
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_cx, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  taps = [1, 0.1, -1.87, 3.02, -1.435, 0.49]
-  pcs = [1,2,3]
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_cx, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  pcs = [1,1,2,3]
-  for slicing in range(5000,200000,5000):
-    for order in range(2, 9):
-      job.apply_async(task_cx, args=(pcs, taps, order, winsize, r, slicing))
-      print "winsize %s for ma(%s)" % (winsize, len(pcs))
-  
-  job.close()
-  job.join()
+    job = Pool(8)
+    r = 50
+    winsize = 512
+
+    # snr = +inf
+    pcs = [1,2,3]
+    for slicing in range(5000,200000,5000):
+        for order in range(2, 7):
+            job.apply_async(task_cx, args=(pcs, order, winsize, r, slicing, 500, "white"))
+            for snr in range(1,101):
+                job.apply_async(task_cx, args=(pcs, order, winsize, r, slicing, snr, "white"))
+                job.apply_async(task_cx, args=(pcs, order, winsize, r, slicing, snr, "color"))
+    pcs = [1,1,2,3]
+    for slicing in range(5000,200000,5000):
+        for order in range(2, 7):
+            job.apply_async(task_cx, args=(pcs, order, winsize, r, slicing, 500, "white"))
+            for snr in range(1,101):
+                job.apply_async(task_cx, args=(pcs, order, winsize, r, slicing, snr, "white"))
+                job.apply_async(task_cx, args=(pcs, order, winsize, r, slicing, snr, "color"))
+    #benchmark for non-PCS
+    for slicing in range(5000,200000,5000):
+        for order in range(2, 7):
+            job.apply_async(task_cm, args=(2, order, winsize, r, slicing, 500, "white"))
+            job.apply_async(task_cm, args=(3, order, winsize, r, slicing, 500, "white"))
+            job.apply_async(task_cm, args=(4, order, winsize, r, slicing, 500, "white"))
+            for snr in range(1,101):
+                job.apply_async(task_cx, args=(2, order, winsize, r, slicing, snr, "white"))
+                job.apply_async(task_cx, args=(3, order, winsize, r, slicing, snr, "white"))
+                job.apply_async(task_cx, args=(4, order, winsize, r, slicing, snr, "white"))
+                job.apply_async(task_cx, args=(2, order, winsize, r, slicing, snr, "color"))
+                job.apply_async(task_cx, args=(3, order, winsize, r, slicing, snr, "color"))
+                job.apply_async(task_cx, args=(4, order, winsize, r, slicing, snr, "color"))
+
+
+    job.close()
+    job.join()
 
 if __name__ == "__main__":
-  main()
-    
+    main()
+
 
