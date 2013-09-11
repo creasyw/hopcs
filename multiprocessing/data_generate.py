fafe820893e855a7e37f003d952d72430fa7f0d6
creasyw <creasywuqiong@gmail.com>
2013-09-10 22:14:58 -0500

    add arma data generation

--- multiprocessing/data_generate.py
+++ multiprocessing/data_generate.py
@@ -67,7 +67,40 @@ def ar_gen():
     job.join()
 
 
+def arma_channel(sig_ar_tap, sig_ma_tap, noise_ar_tap, noise_ma_tap, i):
+    signal = np.load("/home/creasy/workplace/data/exp_deviate_one_%d.npy"%(i))[:60000]
+    receive = lfilter(sig_ma_tap, sig_ar_tap, signal)
+    # save signal snr=+inf
+    np.save("temp/ar_data_%d.npy"%(i), receive)
+    sig_power = (sum(sig_ar_tap)+sum(sig_ma_tap))**2
+    noise_power = (sum(noise_ar_tap)+sum(noise_ma_tap))**2
+
+    # snr = 0..20 (scale from 1 to 100)
+    for j in range(0,21):
+        amp = 10**(j/10.)
+        white = np.random.normal(0, sqrt(sig_power/amp), len(signal))
+        color_scale = sqrt(sig_power/amp/noise_power)
+        color = lfilter(noise_ma_tap, noise_ar_tap, np.random.normal(0, color_scale, len(signal)))
+        np.save("temp/arma_data_white_%d_%d.npy"%(j, i), white+receive)
+        np.save("temp/arma_data_color_%d_%d.npy"%(j, i), color+receive)
+
+def arma_gen():
+    job = Pool(8)
+    r = 50
+    sig_ar_tap = [1, -2.2, 1.77, -0.52]
+    sig_ma_tap = [1, -1.25]
+    noise_ar_tap = [1, 0.3, 0.745]
+    noise_ma_tap = [1, -2.33, 0.75, 0.5, 0.3, -1.41]
+
+    for i in range(r):
+        job.apply_async(arma_channel, args=(sig_ar_tap, sig_ma_tap, noise_ar_tap, noise_ma_tap, i))
+        print "The %d Monte Carlo data prepared"%(i)
+
+    job.close()
+    job.join()
+
+
 if __name__ == "__main__":
-    ar_gen()
+    arma_gen()
 
 
