89513a5bd796d59e302cb21b584ada7ff2f4f6b6
creasyw <creasywuqiong@gmail.com>
2013-10-24 15:08:35 -0500

    add randome matrix to prove Toepliz

--- matrix_derive/right_circulant.py
+++ matrix_derive/right_circulant.py
@@ -7,7 +7,14 @@ def display(n=10):
     # build causal right circulant matrix
     # np.matrix rather than np.ndarray could perform matrix mulitplication
     s1 = np.matrix(np.flipud(np.fliplr(circulant(s1)[n-1:,:])))
-    s2 = np.matrix(s1)
+    #s2 = np.matrix(s1).T
+    # using a diffrent and random matrix for generality
+    s2 = [np.random.randint(1,20) for i in range(n)]+[0 for i in range(n-1)]
+    s2 = np.matrix(np.flipud(np.fliplr(circulant(s2)[n-1:,:]))).T
+
+    print s1
+    print "\n", s2
+    print "\n", s1*s2
 
 if __name__ == "__main__":
     display()
