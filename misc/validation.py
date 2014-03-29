6cddf2ef9703128eaeb699ecbcb4c5e16112d590
creasyw <creasywuqiong@gmail.com>
2014-03-29 16:25:47 -0500

    update validation

--- misc/validation.py
+++ misc/validation.py
@@ -1,7 +1,8 @@
 import cumxst as cx
 import nested_cumxst as ncx
 import numpy as np
-import nested_maest as ma
+import nested_maest as nma
+import maest as ma
 import impulse_response as ir
 
 winsize = 512
@@ -21,3 +22,20 @@ signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
 receive = ir.moving_average(taps, signal)
 pcs = [1,1,1]
 print "With PCS downsampling:", cx.cumx(receive, pcs, len(pcs), len(taps)-1, winsize)
+
+#######################
+
+signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
+receive = ir.moving_average(taps, signal)
+pcs = [4,3,4]
+print "With nested sampling:", nma.maestx (receive, pcs, len(taps)-1, len(pcs), winsize)
+
+signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
+receive = ir.moving_average(taps, signal)
+pcs = [2,3,5]
+print "Without downsampling:", ma.maestx (receive, pcs, len(taps)-1, len(pcs), winsize)
+
+signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
+receive = ir.moving_average(taps, signal)
+pcs = [1,1,1]
+print "With PCS downsampling:", ma.maestx (receive, pcs, len(taps)-1, len(pcs), winsize)
