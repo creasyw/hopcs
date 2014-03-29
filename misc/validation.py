2b54d53ef9d3bb64be0c0742b1f73d2ba89402fe
creasyw <creasywuqiong@gmail.com>
2014-03-29 15:39:53 -0500

    update validation func

--- misc/validation.py
+++ misc/validation.py
@@ -3,12 +3,20 @@ import nested_cumxst as ncx
 import numpy as np
 import impulse_response as ir
 
+winsize = 512
+taps = [1, -2.333, 0.667]
+
 signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
 receive = ir.moving_average(taps, signal)
-
-win = 512
 nl = [4,3,4]
-t1 = ncx.cum2x(receive, nl, len(taps)-1, winsize, 0)
+print "With nested sampling:", ncx.cumx(signal, nl, len(nl), len(taps)-1, winsize)
 
-pcs = [2,3]
-t2 = = cx.cumx(receive, pcs, len(pcs), len(taps)-1, winsize)
+signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
+receive = ir.moving_average(taps, signal)
+pcs = [2,3,5]
+print "Without downsampling:", cx.cumx(signal, pcs, len(pcs), len(taps)-1, winsize)
+
+signal = np.load("../data/exp_deviate_one_0.npy")[:10000]
+receive = ir.moving_average(taps, signal)
+pcs = [1,1,1]
+print "With PCS downsampling:", cx.cumx(signal, pcs, len(pcs), len(taps)-1, winsize)
